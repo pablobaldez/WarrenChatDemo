72a8f9a33869282dd1e0ce032d1edcffe55e4c83
DESKTOP-3V7SKAU\pablo <pablo_baldez@outlook.com>
2018-04-24 04:39:31 -0300

    fixing user initial, removing libraries and unused code

--- app/src/main/kotlin/com/github/pablo/warrenchatdemo/views/base/ViewExtensions.kt
+++ app/src/main/kotlin/com/github/pablo/warrenchatdemo/views/base/ViewExtensions.kt
@@ -1,83 +1,29 @@
 package com.github.pablo.warrenchatdemo.views.base
 
-import android.databinding.BindingAdapter
-import android.support.v4.view.GravityCompat
-import android.support.v7.widget.CardView
-import android.view.Gravity
-import android.view.View
-import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
+import android.text.InputType
 import android.widget.EditText
-import android.widget.LinearLayout
 import android.widget.TextView
 import com.github.pablo.warrenchatdemo.R
 
-@BindingAdapter("isUserChatTextColor")
-fun TextView.setUserChatTextColor(chatMessageFromUser: Boolean) {
-    val colorRes = if(chatMessageFromUser) {
-        R.color.black
-    } else {
-        R.color.white
-    }
-    val color = resources.getColorCodeFromRes(colorRes)
-    setTextColor(color)
-}
-
-@BindingAdapter("isUserChatBackground")
-fun View.setUserChatBackground(chatMessageFromUser: Boolean) {
-    val colorRes = if(chatMessageFromUser) {
-        R.color.light_gray
-    } else {
-        R.color.colorPrimary
-    }
-    val color = resources.getColorCodeFromRes(colorRes)
-    if(this is CardView) {
-        setCardBackgroundColor(color)
-    } else {
-        setBackgroundColor(color)
-    }
-}
-
-@BindingAdapter("isUserChatMargin")
-fun View.setUserChatMargin(chatMessageFromUser: Boolean) {
-    val margin = resources.getDimensionPixelSize(R.dimen.chat_message_horizontal_margin)
-    val newLayoutParams = layoutParams
-    if(newLayoutParams is ViewGroup.MarginLayoutParams) {
-        if(chatMessageFromUser) {
-            newLayoutParams.marginStart += margin
-        } else {
-            newLayoutParams.marginEnd += margin
-        }
-    }
-}
-
-@BindingAdapter("isUserChatGravity")
-fun LinearLayout.setUserChatGravity(chatMessageFromUser: Boolean) {
-    gravity = if(chatMessageFromUser) {
-        GravityCompat.END or Gravity.TOP
-    } else {
-        GravityCompat.START or Gravity.TOP
-    }
-}
-
 fun EditText.setupNameMask() {
-    inputType = EditorInfo.TYPE_TEXT_FLAG_CAP_WORDS or
-            EditorInfo.TYPE_TEXT_VARIATION_PERSON_NAME
+    inputType = InputType.TYPE_CLASS_TEXT or
+            InputType.TYPE_TEXT_FLAG_CAP_WORDS or
+            InputType.TYPE_TEXT_VARIATION_PERSON_NAME
     setHint(R.string.your_name)
 }
 
 fun EditText.setupEmailMask() {
-    inputType = EditorInfo.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+    inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
     setHint(R.string.your_email)
 }
 
 fun EditText.setupCurrencyMask() {
-    inputType = EditorInfo.TYPE_NUMBER_FLAG_DECIMAL
+    inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
     setHint(R.string.your_email)
 }
 
 fun EditText.setupIntegerMask() {
-    inputType = EditorInfo.TYPE_NUMBER_VARIATION_NORMAL
+    inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_NORMAL
     hint = ""
 }
 
